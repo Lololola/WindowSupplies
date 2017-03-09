24a83532fd706b1d622401c9e46e1fdf5141d464
Lololola <lolarun314@gmail.com>
2017-03-08 21:51:30 -0500

    added minimum and maximum measurements for width and height

--- WindowSupplyApp/Program.cs
+++ WindowSupplyApp/Program.cs
@@ -24,12 +24,40 @@ namespace WindowSupplyApp
         {
             if (true)
             {
+                const double MAX_WIDTH = 5.0;
+                const double MIN_WIDTH = 0.5;
+                const double MAX_HEIGHT = 3.0;
+                const double MIN_HEIGHT = 0.75;
+
                 Console.Write("Type in Window Height (in meters): ");
                 float windowHeight = float.Parse(Console.ReadLine());
+                if (windowHeight < MIN_HEIGHT)
+                {
+                    Console.WriteLine("Height is too small.\n\n");
+                    Console.WriteLine("Using minimum: ");
+                    windowHeight = (float)MIN_HEIGHT;
+                }
+                if (windowHeight > MAX_HEIGHT)
+                {
+                    Console.WriteLine("Height is too large.\n\n");
+                    Console.WriteLine("Using maximum: ");
+                    windowHeight = (float)MAX_HEIGHT;
+                }
                 Console.WriteLine();
                 Console.Write("Type in Window Width (in meters): ");
                 float windowWidth = float.Parse(Console.ReadLine());
-               
+                if (windowWidth < MIN_WIDTH)
+                {
+                    Console.WriteLine("Width is too small.\n\n");
+                    Console.WriteLine("Using minimum");
+                    windowWidth = (float)MIN_WIDTH;
+                }
+                if (windowWidth > MAX_HEIGHT)
+                {
+                    Console.WriteLine("Width is too large.\n\n");
+                    Console.WriteLine("Using maximum");
+                    windowWidth = (float)MAX_WIDTH;
+                }
                 Console.WriteLine();
 
                 float totalWood = WoodCalc.TotalWoodLength(
